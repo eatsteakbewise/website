3f43eb99c0f14d0203643793cbeb3ec32d0c1645
brianchilders <brian@childers.org>
2021-06-01 16:10:41 -0500

    added coins.js

--- js/farming.js
+++ js/farming.js
@@ -1,9 +1,6 @@
 // Global variables
 alert("Thank you for visiting today!");
 
-let current_price_ethereum = 0;
-let current_price_swise = 0;
-let current_price_1inch = 0;
 let swap_ethereum_swise = 0;
 let swap_ethereum_1inch = 0;
 let amount_swise_invest = 0;
@@ -35,54 +32,6 @@ let swise_1inch_ratio = 0;
 // value_swise
 // cost_1inch
 
-// create function to parse json from a URL and store data
-	var getJSON = function(url, callback) {
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, true);
-			xhr.responseType = 'json';
-			xhr.onload = function() {
-				var status = xhr.status;
-				if (status === 200) {
-				callback(null, xhr.response);
-				} else {
-				callback(status, xhr.response);
-				}
-			};
-			xhr.send();
-	};
-
-	// Get Stakewise data
-	getJSON('https://api.coingecko.com/api/v3/coins/stakewise?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=true',
-	function(err, data) {
-		if (err !== null) {
-				alert('Something went wrong: ' + err);
-		} else {
-				current_price_swise = data.market_data.current_price.usd;
-			currprice_swise.innerHTML = current_price_swise;
-		}
-	});
-
-	// Get 1 inch data
-	getJSON('https://api.coingecko.com/api/v3/coins/1inch?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=true',
-	function(err, data) {
-		if (err !== null) {
-				alert('Something went wrong: ' + err);
-		} else {
-				current_price_1inch = data.market_data.current_price.usd;
-			currprice_1inch.innerHTML = current_price_1inch;
-		}
-	});
-
-	// Get Ethereum data
-	getJSON('https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=true',
-	function(err, data) {
-		if (err !== null) {
-				alert('Something went wrong: ' + err);
-		} else {
-				current_price_ethereum = data.market_data.current_price.usd;
-			currprice_ethereum.innerHTML = current_price_ethereum;
-		}
-	});
 
 	// Set startdate and enddate on page load
 	window.onload = function() {
